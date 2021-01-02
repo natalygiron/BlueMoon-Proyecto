f29003012966f831d0cac74fdc3d2fc0c72acc45
NATALY <natalygiron18@outlook.com>
2021-01-01 21:57:45 -0500

    bluemoonproyecto v1

---
+++ src/bluemoonproyecto/Login.java
@@ -0,0 +1,178 @@
+
+package bluemoonproyecto;
+
+
+import bluemoonproyecto.model.Conexion;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import javax.swing.JOptionPane;
+
+
+public class Login extends javax.swing.JFrame {
+
+    MenuAdmin admin = new MenuAdmin();
+    MenuMozo mozo = new MenuMozo();
+    MenuCajero cajero = new MenuCajero();
+    
+    public Login() {
+        initComponents();
+        Conexion.getConexion();
+    }
+
+    
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        txtEmail = new javax.swing.JTextField();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        txtPassword = new javax.swing.JTextField();
+        jButton1 = new javax.swing.JButton();
+        jPanel1 = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        jLabel1.setText("Email:");
+
+        jLabel2.setText("Password:");
+
+        jButton1.setText("Ingresar");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
+        javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
+        jPanel1.setLayout(jPanel1Layout);
+        jPanel1Layout.setHorizontalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 143, Short.MAX_VALUE)
+        );
+        jPanel1Layout.setVerticalGroup(
+            jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 153, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(185, 185, 185)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txtPassword, javax.swing.GroupLayout.DEFAULT_SIZE, 143, Short.MAX_VALUE)
+                    .addComponent(jLabel2)
+                    .addComponent(jLabel1)
+                    .addComponent(txtEmail))
+                .addContainerGap(158, Short.MAX_VALUE))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                .addGap(40, 40, 40)
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jLabel1)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(txtEmail, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(18, 18, 18)
+                .addComponent(jLabel2)
+                .addGap(18, 18, 18)
+                .addComponent(txtPassword, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(28, 28, 28)
+                .addComponent(jButton1, javax.swing.GroupLayout.PREFERRED_SIZE, 39, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(42, 42, 42))
+        );
+
+        pack();
+    }// </editor-fold>//GEN-END:initComponents
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        PreparedStatement ps;
+        ResultSet rs;
+
+        String email = txtEmail.getText();
+        String password = txtPassword.getText();
+
+        try {
+
+            Connection con = Conexion.getConexion();
+            ps = con.prepareStatement("SELECT EMAIL_USUARIO, CONTRASEÑA_USUARIO, TIPO_USUARIO FROM USUARIO WHERE EMAIL_USUARIO =?");
+            ps.setString(1, email);
+            rs = ps.executeQuery();
+            
+            
+            if(rs.next() == false) {
+                JOptionPane.showMessageDialog(null, "Usuario o pssword incorrecto.","Error", JOptionPane.ERROR_MESSAGE);
+            }else{
+                do{
+                    if(email.equals(rs.getString("EMAIL_USUARIO")) && password.equals(rs.getString("CONTRASEÑA_USUARIO"))){
+                        String tipoUsuario = rs.getString("TIPO_USUARIO");
+                        switch(tipoUsuario){
+                            case "admin": admin.setVisible(true); break;
+                            case "mozo": mozo.setVisible(true); break;
+                            case "cajero": cajero.setVisible(true); break;
+                        }
+                    }
+                }while(rs.next());  
+            }
+
+            //System.out.println(rs);
+
+        } catch (Exception e) {
+            System.out.println("ERROR");
+        }
+    }//GEN-LAST:event_jButton1ActionPerformed
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(Login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new Login().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JPanel jPanel1;
+    private javax.swing.JTextField txtEmail;
+    private javax.swing.JTextField txtPassword;
+    // End of variables declaration//GEN-END:variables
+}
---
+++ src/bluemoonproyecto/model/Conexion.java
@@ -0,0 +1,53 @@
+
+package bluemoonproyecto.model;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import javax.swing.JOptionPane;
+
+public class Conexion {
+    static Connection contacto = null;
+        
+    public static Connection getConexion(){
+            
+            String connectionUrl ="jdbc:sqlserver://localhost\\SQLEXPRESS:1433;databaseName=BLUEMOONFINAL;user=sa;password=123456;";
+            //jdbc:sqlserver://localhost\SQLEXPRESS:1433;databaseName=BLUEMOONFINAL;
+            try {
+                Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");//conecta con el driver
+            }
+            // Handle any errors that may have occurred.
+            catch (ClassNotFoundException e) {
+                JOptionPane.showMessageDialog(null, "No se pudo establecer conexion con el driver "+ e.getMessage(),
+                        "Error de conexion",JOptionPane.ERROR_MESSAGE);
+            }
+            
+            try {
+                contacto = DriverManager.getConnection(connectionUrl);
+                JOptionPane.showMessageDialog(null, "Conexión establecida");
+            } catch (SQLException e) {
+                JOptionPane.showMessageDialog(null, "Error "+e.getMessage(),
+                        "Error de conexion", JOptionPane.ERROR_MESSAGE);
+            }
+            
+            return contacto;
+        }
+    
+    //recibe las consultas y nos devolvera los resultados
+    public static ResultSet Consulta(String consulta){
+        Connection con = getConexion();//conectar con la BD
+        Statement declara;//representa una instruccion
+        try {
+            declara = con.createStatement();
+            ResultSet respuesta = declara.executeQuery(consulta); 
+            return respuesta;
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Error "+e.getMessage(),
+                        "Error de conexion", JOptionPane.ERROR_MESSAGE);
+        }
+        return null;
+    }
+    
+}
